5a824ac18d20126278dbd5c3e0f40f760c67efec
Ciprian Ionescu <me@ciprian-ionescu.com>
2020-05-10 02:27:36 +0300

    bootstrap

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,10 +1,12 @@
 import { h } from 'preact';
 import style from './style';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => (
 	<div class={style.home}>
 		<h1>Home</h1>
 		<p>This is the Home component.</p>
+		<Button>Test</Button>
 	</div>
 );
 
